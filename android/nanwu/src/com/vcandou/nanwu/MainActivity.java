91a1f84883ccfe500d82f2c09d5e7489097e18b9
carlleton <carlleton@hotmail.com>
2015-07-27 01:19:59 +0800

    开始nanwu自己做一个小项目

---
+++ android/nanwu/src/com/vcandou/nanwu/MainActivity.java
@@ -0,0 +1,51 @@
+package com.vcandou.nanwu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		init();
+	}
+	
+	private void init(){
+		int shownum=R.string.show_num;
+		LinearLayout ly= (LinearLayout) findViewById(R.id.liner1);
+		
+		
+		char[] basestr_chars=this.getString(R.string.basestr).toCharArray();
+		int base_n=basestr_chars.length;
+		String[] basestrs=new String[base_n];
+		int base_i=0;
+		
+		for(int i=0;i<base_n;i++){
+			basestrs[i]= String.valueOf(basestr_chars[i]);
+		}
+		
+		System.out.println("basen:"+base_n);
+		
+		for(int i=0;i<base_n;i++){
+			System.out.println("basen:+"+basestrs[i]);
+		}
+		
+		LayoutParams lps=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		//for(int i=0;i<shownum;i++){
+			TextView textview1 = new TextView(this);
+			textview1.setText("ÄÏÎÞ°¢ÃÖÍÓ·ð");//basestrs[base_i]
+			
+		//	base_i++;
+		//	if(base_i>=base_n)base_i=0;
+			ly.addView(textview1,lps);
+		//}
+		
+	}
+
+}
