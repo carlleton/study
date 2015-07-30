eb252e1d592f32d21ac0fb587b971fb5fa089b34
carlleton <carlleton@hotmail.com>
2015-07-30 17:48:08 +0800

    add study underscore

---
+++ underscore/string.js
@@ -0,0 +1,6 @@
+var _=require('underscore');
+_.str=require('underscore.string');
+//_.mixin(_.str.exports());
+
+console.log(_.str.capitalize('epeli'));
+console.log(_.str.include('Underscore.string','string'));
