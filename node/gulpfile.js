ca2c95e03eecf8909ad8854342fba5e1f201b35a
carlleton <carlleton@hotmail.com>
2016-01-18 15:22:44 +0800

    学习gulp，增加打包zip，计算md5，顺序执行

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -5,7 +5,10 @@ var version = 'V2.1.1.13';
 var outputFolder = 'E:/{version}/project';
 
 //只复制不压缩的文件，以,分隔
+var srcFolder='proName';
 var copyFiles = 'js/config.js,*.doc';
+var md5File = 'MD5.txt';
+var zipFile = 'proName.zip';
 
 var gulp = require('gulp'),					//基础库
 	clean = require('gulp-clean'),			//清理
@@ -16,7 +19,11 @@ var gulp = require('gulp'),					//基础库
 	htmlmin = require('gulp-htmlmin'),		//压缩html
 	imagemin = require('gulp-imagemin'),	//压缩图片
 	pngquant = require('imagemin-pngquant'),//png压缩
-	notify = require('gulp-notify');		//命令行提示
+	zip = require('gulp-zip'),				//打包成zip
+	notify = require('gulp-notify'),		//命令行提示
+	gulpSequence = require('gulp-sequence');//顺序执行任务
+var crypto = require('crypto');
+	var fs = require('fs');
 
 var files_js=['js/*.js'];
 var files_css=['css/*.css'];
@@ -24,6 +31,8 @@ var files_images=['images/*.{png,jpg,gif,ico}'];
 var files_html=['*.html'];
 var files_copy=[];
 outputFolder=outputFolder.replace('{version}',version);
+srcFolder=outputFolder+'/'+srcFolder;
+
 var files=copyFiles.split(',');
 for(var i=0,n=files.length,ext;i<n;i++){
 	ext=getExt(files[i]);
@@ -43,8 +52,12 @@ gulp.task('clean',function(){
 	return gulp.src([outputFolder+'/*'],{read: false})
 		.pipe(clean({force: true}));
 });
+gulp.task('cleanSRC',function(){
+	return gulp.src(srcFolder,{read:false})
+		.pipe(clean({force:true}));
+});
 gulp.task('check',function(){
-	gulp.src('js/*.js')
+	return gulp.src('js/*.js')
 		.pipe(jshint({
 			eqeqeq:true,
 			laxbreak:true
@@ -52,17 +65,17 @@ gulp.task('check',function(){
         .pipe(jshint.reporter('default'));
 });
 gulp.task('minijs',function(){
-	gulp.src(files_js)
+	return gulp.src(files_js)
 		.pipe(uglify())
-		.pipe(gulp.dest(outputFolder+'/js'));
+		.pipe(gulp.dest(srcFolder+'/js'));
 });
 gulp.task('minifycss',function(){
-	gulp.src(files_css)
+	return gulp.src(files_css)
 		.pipe(minifycss())
-		.pipe(gulp.dest(outputFolder+'/css'));
+		.pipe(gulp.dest(srcFolder+'/css'));
 });
 gulp.task('miniimage',function(){
-	gulp.src(files_images)
+	return gulp.src(files_images)
 		.pipe(imagemin({
 			optimizationLevel: 3,//默认3，取值0~7
 			progressive: true,	//默认false，无损压缩jpg
@@ -70,8 +83,8 @@ gulp.task('miniimage',function(){
 			multipass: true, 	//默认false，多次优化svg直到完全优化
 			use:[pngquant()]	//使用pngquant深度压缩png图片的imagemin插件
 		}))
-		.pipe(gulp.dest(outputFolder+'/images'))
-		.pipe(notify({ message: 'Images task complete' }));
+		.pipe(gulp.dest(srcFolder+'/images'));
+		//.pipe(notify({ message: 'Images task complete' }));
 });
 gulp.task('minihtml',function(){
 	var options = {
@@ -88,19 +101,43 @@ gulp.task('minihtml',function(){
 		minifyJS: true,//压缩页面JS
 		minifyCSS: true//压缩页面CSS
 	};
-	gulp.src(files_html)
+	return gulp.src(files_html)
 		.pipe(htmlmin(options))
-		.pipe(gulp.dest(outputFolder));
+		.pipe(gulp.dest(srcFolder));
 });
 gulp.task('copy',function(){
-	gulp.src(files_copy,{base:'.'})
-		.pipe(gulp.dest(outputFolder))
-		.pipe(notify({message:'copy...'}));
+	return gulp.src(files_copy,{base:'.'})
+		.pipe(gulp.dest(srcFolder));
+		//.pipe(notify({message:'copy...'}));
 });
+
+/**压缩成zip**/
+gulp.task('zip',function(){
+	return gulp.src([srcFolder+'*/**'])
+		.pipe(zip(outputFolder+'/'+zipFile))
+		.pipe(gulp.dest(outputFolder));
+});
+/**计算md5**/
+gulp.task('md5',function(){
+	//读取一个Buffer
+	var buffer = fs.readFileSync(outputFolder+'/'+zipFile);
+	var fsHash = crypto.createHash('md5');
+	fsHash.update(buffer);
+	var md5 = fsHash.digest('hex').toLocaleUpperCase();
+	console.log("文件的MD5是：%s", md5.toLocaleUpperCase());
+	
+	var str=md5+'	'+zipFile;
+	fs.writeFile(outputFolder+'/'+md5File,str,function(err,stats){
+		if(err)return console.error('open zip error'+err);
+		return '';
+	});
+});
+
 /**总task调用其他的task**/
-gulp.task('default',['clean'],function(){
-	gulp.start('minijs','minifycss','miniimage','minihtml','copy');
+gulp.task('default',['clean'],function(cb){
+	gulpSequence(['minijs','minifycss','miniimage','minihtml','copy'],'zip','cleanSRC','md5')(cb);
 });
+
 /**获取扩展名**/
 function getExt(fileName){
 	var exts = fileName.split('.');
