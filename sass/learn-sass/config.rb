410e88b027105bb3156bd022a5f0b5cee841249f
carlleton <carlleton@hotmail.com>
2016-01-24 22:54:45 +0800

    学习sass

--- sass/learn-sass/config.rb
+++ sass/learn-sass/config.rb
@@ -1,5 +1,6 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'compass-normalize'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -10,6 +11,7 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style=:expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
--- sass/readme.md
+++ sass/readme.md
@@ -1,2 +1,3 @@
 study from:
 * [Sass和Compass必备技能之Sass篇](http://www.imooc.com/learn/364)
+* [Sass和Compass必备技能之Compass](http://www.imooc.com/learn/371)
