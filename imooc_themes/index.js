8f573361257664ec136c0a3179e99002ad7614a6
carlleton <carlleton@hotmail.com>
2016-01-25 13:38:28 +0800

    调整样式，同时加上监听url

--- imooc_themes/index.js
+++ imooc_themes/index.js
@@ -1,37 +1,61 @@
-/**获取传入参数**/
-function getUrlParam(name) {
-	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-	var r = window.location.search.substr(1).match(reg);
-	if (r != null) return unescape(r[2]); return null;
-}
-/**计算iframe的宽高**/
-function resizeIframe(){
-	var w=$(window).width()-10;
-	var h=$(window).height()-10;
-	$("#iframe1").attr({'width':w,'height':h});
-}
-$(document).ready(function(){
-	resizeIframe();
-	$(window).resize(resizeIframe);
-	
-	var url=getUrlParam('url');
-	$("#iframe1").attr('src',url);
+(function(){
+	var config={
+		firstTimeout:3*1000,
+		timeout:1*1000,
+		isChanging:false,
+		url:''
+	};
 	var cssData='';
-	$.ajax({
-		url:'index.css',
-		success:function(data){
-			cssData=data;
-			setTimeout(function(){
-				setIframeCSS(cssData);
-			},5000);
-		}
+	$(document).ready(function(){
+		resizeIframe();
+		$(window).resize(resizeIframe);
+		var iframe=$(window.frames["iframe1"].document);
+		var url=getUrlParam('url');
+		$("#iframe1").attr('src',url);
+		
+		$.ajax({
+			url:'index.css',
+			success:function(data){
+				cssData=data;
+				setInterval(function(){
+					var url=iframe.context.URL;
+					if(!config.isChanging && url!==config.url){
+						config.isChanging=true;
+						setTimeout(function(){
+							setIframeCSS();
+							config.isChanging=false;
+						},config.firstTimeout);
+					}
+				},config.timeout);
+			}
+		});
 	});
-});
-function setIframeCSS(data){
-	var iframe=$(window.frames["iframe1"].document);
-	var body=iframe.find("body");
-	var head=iframe.find("head");
-	var csstag='<style type="text/css">'+data+'</style>';
-	head.html(head.html()+csstag);
-	body.attr("class","theme");
-}
+	/**获取传入参数**/
+	function getUrlParam(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) return unescape(r[2]); return null;
+	}
+	/**计算iframe的宽高**/
+	function resizeIframe(){
+		var w=$(window).width()-10;
+		var h=$(window).height()-10;
+		$("#iframe1").attr({'width':w,'height':h});
+	}
+	//设置iframe中的样式
+	function setIframeCSS(){
+		var iframe=$(window.frames["iframe1"].document);
+		var url=iframe.context.URL;
+		var head=iframe.find("head");
+		var body=iframe.find("body");
+		var title=iframe.find("title");
+		var csstag='<style type="text/css">'+cssData+'</style>';
+		//head.html(head.html()+csstag);
+		body.append(csstag)
+		config.url=url;
+		
+		$(document).attr("title",title.text());
+		window.history.pushState(null,null,'index.html?url='+url);
+		
+	}
+})();
