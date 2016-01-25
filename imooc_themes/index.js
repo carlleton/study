5898d2172d85a481afc9923106ba0c4d71c65c21
carlleton <carlleton@hotmail.com>
2016-01-25 10:26:31 +0800

    简单的修改了一点点样式。简单的看了看，应该可以通过ace的API更改样式。

--- imooc_themes/index.js
+++ imooc_themes/index.js
@@ -1,33 +1,37 @@
-
+/**获取传入参数**/
 function getUrlParam(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 	var r = window.location.search.substr(1).match(reg);
 	if (r != null) return unescape(r[2]); return null;
 }
+/**计算iframe的宽高**/
 function resizeIframe(){
-	var w=$(window).width();
-	var h=$(window).height();
-	$("#iframe").attr({'width':w,'height':h});
+	var w=$(window).width()-10;
+	var h=$(window).height()-10;
+	$("#iframe1").attr({'width':w,'height':h});
 }
 $(document).ready(function(){
 	resizeIframe();
 	$(window).resize(resizeIframe);
 	
 	var url=getUrlParam('url');
-	$("#iframe").attr('src',url);
+	$("#iframe1").attr('src',url);
 	var cssData='';
 	$.ajax({
 		url:'index.css',
 		success:function(data){
 			cssData=data;
-			setIframeCSS(cssData);
+			setTimeout(function(){
+				setIframeCSS(cssData);
+			},5000);
 		}
 	});
 });
 function setIframeCSS(data){
-	var body=$("#iframe").contents().find("body");
-	var head=$("#iframe").contents().find("head");
+	var iframe=$(window.frames["iframe1"].document);
+	var body=iframe.find("body");
+	var head=iframe.find("head");
 	var csstag='<style type="text/css">'+data+'</style>';
 	head.html(head.html()+csstag);
-	body.attr("css","theme");
+	body.attr("class","theme");
 }
