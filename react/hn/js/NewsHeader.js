20a5abf11872c8a865566e3de70e7b86b1a5a641
carlleton <carlleton@hotmail.com>
2016-02-03 13:06:24 +0800

    react,hn,lesson06，增加header中的nav

--- react/hn/js/NewsHeader.js
+++ react/hn/js/NewsHeader.js
@@ -1,5 +1,6 @@
 var $=require('jquery');
 var React=require('react');
+var _=require('lodash');
 
 var NewsHeader=React.createClass({
 	getLogo:function(){
@@ -16,11 +17,51 @@ var NewsHeader=React.createClass({
 			</div>
 		);
 	},
+	getNav:function(){
+		var navLinks=[
+		{
+			name:'new',
+			url:'newest'
+		},
+		{
+			name:'comments',
+			url:'newcomments'
+		},
+		{
+			name:'show',
+			url:'show'
+		},
+		{
+			name:'ask',
+			url:'ask'
+		},
+		{
+			name:'jobs',
+			url:'jobs'
+		},
+		{
+			name:'submit',
+			url:'submit'
+		}
+		];
+		return (
+			<div className="newsHeader-nav">
+			{_(navLinks).map(function(navLink){
+					return (
+						<a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={'https://news.ycombinator.com/'+navLink.url}>
+							{navLink.name}
+						</a>
+					);
+				}).value()}
+			</div>
+		);
+	},
 	render:function(){
 		return (
 			<div className="newsHeader">
 				{this.getLogo()}
 				{this.getTitle()}
+				{this.getNav()}
 			</div>
 		);
 	}
