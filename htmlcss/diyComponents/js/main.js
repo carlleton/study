8217432ff058ce6469797d81a40c57d3bc6f7f5b
carlleton <carlleton@hotmail.com>
2016-02-24 16:40:15 +0800

    宽和高的参数设置

--- htmlcss/diyComponents/js/main.js
+++ htmlcss/diyComponents/js/main.js
@@ -7,6 +7,10 @@ require(['jquery','Window'],function($,w){
 	$("#a").click(function(){
 		new w.Window().alert("welcome",function(){
 			alert('clicked the button');
+		},{
+			width:300,
+			height:150,
+			y:50
 		});
 	});
 });
--- htmlcss/diyComponents/js/window.js
+++ htmlcss/diyComponents/js/window.js
@@ -1,7 +1,12 @@
 define(['jquery'],function($){
-	function Window(){}
+	function Window(){
+		this.cfg={
+			width:500,
+			height:300
+		}
+	}
 	Window.prototype={
-		alert:function(content,handler){
+		alert:function(content,handler,cfg){
 			var boundingBox=$('<div class="window_boundingBox"></div>');
 			boundingBox.appendTo("body");
 			boundingBox.html(content);
@@ -10,6 +15,13 @@ define(['jquery'],function($){
 			btn.click(function(){
 				handler&&handler();
 				boundingBox.remove();
+			});
+			$.extend(this.cfg,cfg);
+			boundingBox.css({
+				width:this.cfg.width+"px",
+				height:this.cfg.height+"px",
+				left:(this.cfg.x||(window.innerWidth-this.cfg.width)/2)+"px",
+				top:(this.cfg.y||(window.innerHeight-this.cfg.height)/2)+"px"
 			});
 		},
 		confirm:function(){},
