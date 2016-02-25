2c19e3ee1bc277d3e6b70e79dc28bea20262e9b1
carlleton <carlleton@hotmail.com>
2016-02-26 00:07:08 +0800

    重构widget抽象类

--- htmlcss/diyComponents/js/widget.js
+++ htmlcss/diyComponents/js/widget.js
@@ -1,6 +1,6 @@
-define(function(){
+define(['jquery'],function($){
 	function Widget(){
-		this.handlers={};
+		this.boundingBox = null;	//属性：最外层容器
 	}
 	Widget.prototype={
 		on:function(type,handler){
@@ -19,6 +19,23 @@ define(function(){
 			}
 			return this;
 		},
+		
+		render:function(container){//方法：渲染组件
+			this.renderUI();
+			this.handlers={};
+			this.bindUI();
+			this.syncUI();
+			$(container||document.body).append(this.boundingBox);
+		},
+		destroy:function(){			//方法：销毁组件
+			this.destructor();
+			this.boundingBox.off();
+			this.boundingBox.remove();
+		},
+		renderUI:function(){},		//接口：添加DOM接口
+		bindUI:function(){},		//接口：监听事件
+		syncUI:function(){},		//接口：初始化组件属性
+		destructor:function(){},	//接口：销毁前的处理函数
 	}
 	return {
 		Widget:Widget
--- htmlcss/diyComponents/js/window.js
+++ htmlcss/diyComponents/js/window.js
@@ -16,61 +16,61 @@ define(['widget','jquery','jqueryUI'],function(widget,$,$UI){
 		}
 	}
 	Window.prototype=$.extend({},new widget.Widget(),{
-		alert:function(cfg){
-			var CFG=$.extend(this.cfg,cfg),
-			boundingBox=$('<div class="window_boundingBox">'+
-				'<div class="window_header">'+CFG.title+'</div>'+
-				'<div class="window_body">'+CFG.content+'</div>'+
-				'<div class="window_footer"><input type="button" class="window_alertBtn" value="'+CFG.text4AlertBtn+'"></div>'+
-				'</div>'),
-			btn=boundingBox.find(".window_alertBtn"),
-			mask=null;
-			that=this;
-
-			if(CFG.hasMask){
-				mask=$('<div class="window_mask"></div>');
-				mask.appendTo("body");
+		renderUI:function(){
+			this.boundingBox=$('<div class="window_boundingBox">'+
+				'<div class="window_header">'+this.cfg.title+'</div>'+
+				'<div class="window_body">'+this.cfg.content+'</div>'+
+				'<div class="window_footer"><input type="button" class="window_alertBtn" value="'+this.cfg.text4AlertBtn+'"></div>'+
+				'</div>');
+			if(this.cfg.hasMask){//遮罩
+				this._mask=$('<div class="window_mask"></div>');
+				this._mask.appendTo("body");
 			}
-
-			boundingBox.appendTo("body");
-			btn.click(function(){
-				boundingBox.remove();
-				mask&&mask.remove();
+			if(this.cfg.hasCloseBtn){
+				this.boundingBox.append('<span class="window_closeBtn">X</span>');
+			}
+			this.boundingBox.appendTo("body");
+		},
+		bindUI:function(){
+			var that=this;
+			this.boundingBox.delegate(".window_alertBtn","click",function(){
 				that.fire("alert");
+				that.destroy();
+			}).delegate(".window_closeBtn","click",function(){
+				that.fire("close");
+				that.destroy();
 			});
-			
-			boundingBox.css({
+			if(this.cfg.handler4AlertBtn){
+				this.on("alert",this.cfg.handler4AlertBtn);
+			}
+			if(this.cfg.handler4CloseBtn){
+				this.on("close",this.cfg.handler4CloseBtn);
+			}
+		},
+		syncUI:function(){
+			this.boundingBox.css({
 				width:this.cfg.width+"px",
 				height:this.cfg.height+"px",
 				left:(this.cfg.x||(window.innerWidth-this.cfg.width)/2)+"px",
 				top:(this.cfg.y||(window.innerHeight-this.cfg.height)/2)+"px"
 			});
-			if(CFG.hasCloseBtn){
-				var closeBtn = $('<span class="window_closeBtn">X</span>');
-				closeBtn.appendTo(boundingBox);
-				closeBtn.click(function(){
-					boundingBox.remove();
-					mask&&mask.remove();
-					that.fire("close");
-				});
+			if(this.cfg.skinClassName){
+				this.boundingBox.addClass(this.cfg.skinClassName);
 			}
-			if(CFG.handler4AlertBtn){
-				this.on("alert",CFG.handler4AlertBtn);
-			}
-			if(CFG.handler4CloseBtn){
-				this.on("close",CFG.handler4CloseBtn);
-			}
-
-			if(CFG.skinClassName){
-				boundingBox.addClass(CFG.skinClassName);
-			}
-			if(CFG.isDraggable){
-				if(CFG.dragHandle){
-					boundingBox.draggable({handle:CFG.dragHandle});
+			if(this.cfg.isDraggable){
+				if(this.cfg.dragHandle){
+					this.boundingBox.draggable({handle:this.cfg.dragHandle});
 				}else{
-					boundingBox.draggable();
+					this.boundingBox.draggable();
 				}
 			}
+		},
+		destructor:function(){
+			this._mask&&this._mask.remove();
+		},
+		alert:function(cfg){
+			$.extend(this.cfg,cfg);
+			this.render();
 			return this;
 		},
 		confirm:function(){},
