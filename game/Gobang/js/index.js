41d709ef7cbb03257e68f0743467624c90511c65
carlleton <carlleton@hotmail.com>
2016-05-22 22:41:39 +0800

    跟随教程完成五子棋的AI实现，有点简单ing

--- game/Gobang/js/index.js
+++ game/Gobang/js/index.js
@@ -1,11 +1,70 @@
 var chessBoard = [];
 var me=true;
+var over=false;
+
+//赢法数组
+var wins=[];
+
+//赢法的统计数组
+var myWin=[];
+var computerWin=[];
+
 for(var i=0;i<15;i++){
 	chessBoard[i]=[];
 	for(var j=0;j<15;j++){
 		chessBoard[i][j]=0;
 	}
 }
+
+for(var i=0;i<15;i++){
+	wins[i]=[];
+	for(var j=0;j<15;j++){
+		wins[i][j]=[];
+	}
+}
+var count=0;
+//所有的横线的赢法
+for(var i=0;i<15;i++){
+	for(var j=0;j<11;j++){
+		for(var k=0;k<5;k++){
+			wins[i][j+k][count]=true;
+		}
+		count++;
+	}
+}
+//所有的竖线的赢法
+for(var i=0;i<15;i++){
+	for(var j=0;j<11;j++){
+		for(var k=0;k<5;k++){
+			wins[j+k][i][count]=true;
+		}
+		count++;
+	}
+}
+//所有的斜线的赢法
+for(var i=0;i<11;i++){
+	for(var j=0;j<11;j++){
+		for(var k=0;k<5;k++){
+			wins[i+k][j+k][count]=true;
+		}
+		count++;
+	}
+}
+//所有的反斜线的赢法
+for(var i=0;i<11;i++){
+	for(var j=14;j>3;j--){
+		for(var k=0;k<5;k++){
+			wins[i+k][j-k][count]=true;
+		}
+		count++;
+	}
+}
+
+for(var i=0;i<count;i++){
+	myWin[i]=0;
+	computerWin[i]=0;
+}
+
 
 var chess=document.getElementById('chess');
 var context=chess.getContext('2d');
@@ -45,18 +104,110 @@ var onStep=function(i,j,me){
 	context.fill();
 }
 chess.onclick=function(e){
+	if(over)return;
+	if(!me)return;
 	var x = e.offsetX;
 	var y=e.offsetY;
 	var i = Math.floor(x/30);
 	var j = Math.floor(y/30);	
 	if(chessBoard[i][j]==0){
 		onStep(i,j,me);
-		if(me){
-			chessBoard[i][j]=1;
-		}else{
-			chessBoard[i][j]=2;
+		chessBoard[i][j]=1;
+	}
+	//
+	
+	for(var k=0;k<count;k++){
+		if(wins[i][j][k]){
+			myWin[k]++;
+			computerWin[k]=6;
+			if(myWin[k]==5){
+				alert("你赢了");
+				over=true;
+			}
 		}
 	}
-	me=!me;
+	if(!over){
+		me=!me;
+		computerAI();
+	}
 }
+var computerAI=function(){
+	var myScore = [];
+	var computerScore=[];
+	var max=0;
+	var u=0,v=0;
+	
+	for(var i=0;i<15;i++){
+		myScore[i]=[];
+		computerScore[i]=[];
+		for(var j=0;j<15;j++){
+			myScore[i][j]=0;
+			computerScore[i][j]=0;
+		}
+	}
+	for(var i=0;i<15;i++){
+		for(var j=0;j<15;j++){
+			if(chessBoard[i][j]==0){
+				for(var k=0;k<count;k++){
+					if(wins[i][j][k]){
+						if(myWin[k]==1){
+							myScore[i][j]+=200;
+						}else if(myWin[k]==2){
+							myScore[i][j]+=400;
+						}else if(myWin[k]==3){
+							myScore[i][j]+=2000;
+						}else if(myWin[k]==4){
+							myScore[i][j]+=10000;
+						}
+						
+						if(computerWin[k]==1){
+							computerScore[i][j]+=220;
+						}else if(computerWin[k]==2){
+							computerScore[i][j]+=420;
+						}else if(computerWin[k]==3){
+							computerScore[i][j]+=2100;
+						}else if(computerWin[k]==4){
+							computerScore[i][j]+=20000;
+						}
+					}
+				}
+				if(myScore[i][j]>max){
+					max=myScore[i][j];
+					u=i;
+					v=j;
+				}else if(myScore[i][j]==max){
+					if(computerScore[i][j]>computerScore[u][v]){
+						u=i;
+						v=j;
+					}
+				}
+				if(computerScore[i][j]>max){
+					max=computerScore[i][j];
+					u=i;
+					v=j;
+				}else if(computerScore[i][j]==max){
+					if(myScore[i][j]>myScore[u][v]){
+						u=i;
+						v=j;
+					}
+				}
+			}
+		}
+	}
+	onStep(u,v,false);
+	chessBoard[u][v]=2;
+	for(var k=0;k<count;k++){
+		if(wins[u][v][k]){
+			computerWin[k]++;
+			myWin[k]=6;
+			if(computerWin[k]==5){
+				alert("计算机赢了");
+				over=true;
+			}
+		}
+	}
+	if(!over){
+		me=!me;
+	}
+};
 drawChessBoard();
